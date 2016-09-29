973a04e0c0872f6abcba6fc4b4270f29e79f7591
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-29 19:10:01 -0400

    Celebrity Class

--- app/src/main/java/com/zenbarrier/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/zenbarrier/guessthecelebrity/MainActivity.java
@@ -1,9 +1,52 @@
 package com.zenbarrier.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    class Celebrity{
+        private URL celebImageUrl;
+        private String celebName;
+
+        public Celebrity(String name, URL imageUrl){
+            celebImageUrl = imageUrl;
+            celebName = name;
+        }
+
+        public Celebrity(String name, String imageUrl){
+            try {
+                celebImageUrl = new URL(imageUrl);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            celebName = name;
+        }
+
+        public String getCelebrityName(){
+            return celebName;
+        }
+
+        public Bitmap getCelebrityBitmap(){
+            try {
+                InputStream inputStream = (InputStream)celebImageUrl.getContent();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                inputStream.close();
+                return bitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
