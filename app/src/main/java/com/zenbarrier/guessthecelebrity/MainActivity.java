80861a3808301ddf9669cbdca80686bfedadda5a
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-29 19:14:02 -0400

    Skeleton for game created

--- app/src/main/java/com/zenbarrier/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/zenbarrier/guessthecelebrity/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -48,9 +49,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void answerClick(View view){
+
+    }
+
+    void extractCelebrities(){
+
+    }
+
+    void createCelebrityQuestion(){
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        extractCelebrities();
+        createCelebrityQuestion();
     }
 }
